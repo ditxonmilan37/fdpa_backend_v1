253774a2bdebdb98b0d078a97aab54806f8115ab
Ditxon G. Milan O <ditxon.milan.contractor@bbva.com>
2023-08-11 03:18:52 -0500

    new changes

--- src/controllers/data_test.controller.ts
+++ src/controllers/data_test.controller.ts
@@ -1,42 +1,42 @@
-import { Request, Response } from 'express'
-import { DataTest_view } from '../entities/data_test_view_all'
+import { Request, Response } from "express";
+import { DataTest_view } from "../entities/data_test_view_all";
 
 export const getDataTestAll = async (req: Request, res: Response) => {
-    try {
+  try {
+    const categories = await DataTest_view.findBy({
+      id_event: parseInt(req.params.idEvent),
+    });
 
-        const categories = await DataTest_view.findBy({ id_event: parseInt(req.params.idEvent) })
-
-        return res.status(200).json({
-            statusBol: true,
-            data: categories
-        })
-
-    } catch (error) {
-        if (error instanceof Error) {
-            return res.status(500).json({
-                statusBol: false,
-                message: error.message
-            })
-        }
+    return res.status(200).json({
+      statusBol: true,
+      data: categories,
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(500).json({
+        statusBol: false,
+        message: error.message,
+      });
     }
-}
+  }
+};
 
 export const getDataTestActive = async (req: Request, res: Response) => {
-    try {
+  try {
+    const categories = await DataTest_view.findBy({
+      id_event: parseInt(req.params.idEvent),
+    });
 
-        const categories = await DataTest_view.findBy({ id_event: parseInt(req.params.idEvent), status: 1 })
-
-        return res.status(200).json({
-            statusBol: true,
-            data: categories
-        })
-
-    } catch (error) {
-        if (error instanceof Error) {
-            return res.status(500).json({
-                statusBol: false,
-                message: error.message
-            })
-        }
+    return res.status(200).json({
+      statusBol: true,
+      data: categories,
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(500).json({
+        statusBol: false,
+        message: error.message,
+      });
     }
-}
+  }
+};
