6005a9054dcc4d366b9759f557b493d83ac17613
Ditxon G. Milan O <ditxon.milan.contractor@bbva.com>
2023-08-11 00:59:00 -0500

    new changes

--- src/entities/results.ts
+++ src/entities/results.ts
@@ -1,153 +1,174 @@
-import { Column, Entity, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, Timestamp, BaseEntity } from "typeorm";
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Timestamp,
+  BaseEntity,
+} from "typeorm";
 
 @Entity("data_results")
 export class Results extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @PrimaryGeneratedColumn()
-    id: number
+  @Column()
+  id_serie: number;
 
-    @Column()
-    id_serie: number
+  @Column()
+  camp1: string;
 
-    @Column()
-    camp1: string
+  @Column()
+  camp2: string;
 
-    @Column()
-    camp2: string
+  @Column()
+  camp3: string;
 
-    @Column()
-    camp3: string
+  @Column()
+  camp4: string;
 
-    @Column()
-    camp4: string
+  @Column()
+  camp5: string;
 
-    @Column()
-    camp5: string
+  @Column()
+  camp6: string;
 
-    @Column()
-    camp6: string
+  @Column()
+  camp7: string;
 
-    @Column()
-    camp7: string
+  @Column()
+  camp8: string;
 
-    @Column()
-    camp8: string
+  @Column()
+  camp9: string;
 
-    @Column()
-    camp9: string
+  @Column()
+  camp10: string;
 
-    @Column()
-    camp10: string
+  @Column()
+  camp11: string;
 
-    @Column()
-    camp11: string
+  @Column()
+  camp12: string;
 
-    @Column()
-    camp12: string
+  @Column()
+  camp13: string;
 
-    @Column()
-    camp13: string
+  @Column()
+  camp14: string;
 
-    @Column()
-    camp14: string
+  @Column()
+  camp15: string;
 
-    @Column()
-    camp15: string
+  @Column()
+  camp16: string;
 
-    @Column()
-    camp16: string
+  @Column()
+  camp17: string;
 
-    @Column()
-    camp17: string
+  @Column()
+  camp18: string;
 
-    @Column()
-    camp18: string
+  @Column()
+  camp19: string;
 
-    @Column()
-    camp19: string
+  @CreateDateColumn()
+  created_at: Date;
 
-    @CreateDateColumn()
-    created_at: Date
+  @UpdateDateColumn()
+  updated_at: Date;
 
-    @UpdateDateColumn()
-    updated_at: Date
+  @Column()
+  status: number;
 
-    @Column()
-    status: number
+  @Column()
+  result1: string;
 
-    @Column()
-    result1: string
+  @Column()
+  result2: string;
 
-    @Column()
-    result2: string
+  @Column()
+  result3: string;
 
-    @Column()
-    result3: string
+  @Column()
+  result4: string;
 
-    @Column()
-    result4: string
+  @Column()
+  result5: string;
 
-    @Column()
-    result5: string
+  @Column()
+  result6: string;
 
-    @Column()
-    result6: string
+  @Column()
+  bol1: number;
 
-    @Column()
-    viento1: string
+  @Column()
+  bol2: number;
 
-    @Column()
-    viento2: string
+  @Column()
+  bol3: number;
 
-    @Column()
-    viento3: string
+  @Column()
+  bol4: number;
 
-    @Column()
-    viento4: string
+  @Column()
+  bol5: number;
 
-    @Column()
-    viento5: string
+  @Column()
+  bol6: number;
 
-    @Column()
-    viento6: string
+  @Column()
+  viento1: string;
 
+  @Column()
+  viento2: string;
+
+  @Column()
+  viento3: string;
+
+  @Column()
+  viento4: string;
+
+  @Column()
+  viento5: string;
+
+  @Column()
+  viento6: string;
+
+  @Column()
+  code: string;
 }
-
 
 @Entity("data_results_m2")
 export class ResultsM2 extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @PrimaryGeneratedColumn()
-    id: number
+  @Column()
+  id_result: number;
 
-    @Column()
-    id_result: number
+  @Column()
+  id_serie: number;
 
-    @Column()
-    id_serie: number
+  @Column()
+  size: string;
 
-    @Column()
-    size: string
+  @Column()
+  r1: string;
 
-    @Column()
-    r1: string
+  @Column()
+  r2: string;
 
-    @Column()
-    r2: string
+  @Column()
+  r3: string;
 
-    @Column()
-    r3: string
+  @Column()
+  validate: string;
 
-    @Column()
-    validate: string
+  @CreateDateColumn()
+  created_at: Date;
 
-
-    @CreateDateColumn()
-    created_at: Date
-
-    @UpdateDateColumn()
-    updated_at: Date
-
-    
-
+  @UpdateDateColumn()
+  updated_at: Date;
 }
--- src/entities/results_view_all.ts
+++ src/entities/results_view_all.ts
@@ -2,268 +2,279 @@ import { BaseEntity, ViewColumn, ViewEntity } from "typeorm";
 
 @ViewEntity("view_data_results_app")
 export class Results_view extends BaseEntity {
+  @ViewColumn()
+  id: number;
 
-    @ViewColumn()
-    id: number
+  @ViewColumn()
+  id_serie: number;
 
-    @ViewColumn()
-    id_serie: number
+  @ViewColumn()
+  camp1: string;
 
-    @ViewColumn()
-    camp1: string
+  @ViewColumn()
+  camp2: string;
 
-    @ViewColumn()
-    camp2: string
+  @ViewColumn()
+  camp3: string;
 
-    @ViewColumn()
-    camp3: string
+  @ViewColumn()
+  camp4: string;
 
-    @ViewColumn()
-    camp4: string
+  @ViewColumn()
+  camp5: string;
 
-    @ViewColumn()
-    camp5: string
+  @ViewColumn()
+  camp6: string;
 
-    @ViewColumn()
-    camp6: string
+  @ViewColumn()
+  camp7: string;
 
-    @ViewColumn()
-    camp7: string
+  @ViewColumn()
+  camp8: string;
 
-    @ViewColumn()
-    camp8: string
+  @ViewColumn()
+  camp9: string;
 
-    @ViewColumn()
-    camp9: string
+  @ViewColumn()
+  camp10: string;
 
-    @ViewColumn()
-    camp10: string
+  @ViewColumn()
+  camp11: string;
 
-    @ViewColumn()
-    camp11: string
+  @ViewColumn()
+  camp12: string;
 
-    @ViewColumn()
-    camp12: string
+  @ViewColumn()
+  camp13: string;
 
-    @ViewColumn()
-    camp13: string
+  @ViewColumn()
+  camp14: string;
 
-    @ViewColumn()
-    camp14: string
+  @ViewColumn()
+  camp15: string;
 
-    @ViewColumn()
-    camp15: string
+  @ViewColumn()
+  camp16: string;
 
-    @ViewColumn()
-    camp16: string
+  @ViewColumn()
+  camp17: string;
 
-    @ViewColumn()
-    camp17: string
+  @ViewColumn()
+  camp18: string;
 
-    @ViewColumn()
-    camp18: string
+  @ViewColumn()
+  camp19: string;
 
-    @ViewColumn()
-    camp19: string
+  @ViewColumn()
+  created_at: Date;
 
-    @ViewColumn()
-    created_at: Date
+  @ViewColumn()
+  updated_at: Date;
 
-    @ViewColumn()
-    updated_at: Date
+  @ViewColumn()
+  status: number;
 
-    @ViewColumn()
-    status: number
+  @ViewColumn()
+  result1: string;
 
-    @ViewColumn()
-    result1: string
+  @ViewColumn()
+  result2: string;
 
-    @ViewColumn()
-    result2: string
+  @ViewColumn()
+  result3: string;
 
-    @ViewColumn()
-    result3: string
+  @ViewColumn()
+  result4: string;
 
-    @ViewColumn()
-    result4: string
+  @ViewColumn()
+  result5: string;
 
-    @ViewColumn()
-    result5: string
+  @ViewColumn()
+  result6: string;
 
-    @ViewColumn()
-    result6: string
+  @ViewColumn()
+  bol1: string;
 
-    @ViewColumn()
-    viento1: string
+  @ViewColumn()
+  bol2: string;
 
-    @ViewColumn()
-    viento2: string
+  @ViewColumn()
+  bol3: string;
 
-    @ViewColumn()
-    viento3: string
+  @ViewColumn()
+  bol4: string;
 
-    @ViewColumn()
-    viento4: string
+  @ViewColumn()
+  bol5: string;
 
-    @ViewColumn()
-    viento5: string
+  @ViewColumn()
+  bol6: string;
 
-    @ViewColumn()
-    viento6: string
+  @ViewColumn()
+  viento1: string;
 
-    @ViewColumn()
-    parcial: string
+  @ViewColumn()
+  viento2: string;
+
+  @ViewColumn()
+  viento3: string;
+
+  @ViewColumn()
+  viento4: string;
+
+  @ViewColumn()
+  viento5: string;
+
+  @ViewColumn()
+  viento6: string;
+
+  @ViewColumn()
+  parcial: string;
 }
 
 @ViewEntity("view_data_results_app_campo")
 export class Results_view_campo extends BaseEntity {
+  @ViewColumn()
+  id: number;
 
-    @ViewColumn()
-    id: number
+  @ViewColumn()
+  id_serie: number;
 
-    @ViewColumn()
-    id_serie: number
+  @ViewColumn()
+  camp1: string;
 
-    @ViewColumn()
-    camp1: string
+  @ViewColumn()
+  camp2: string;
 
-    @ViewColumn()
-    camp2: string
+  @ViewColumn()
+  camp3: string;
 
-    @ViewColumn()
-    camp3: string
+  @ViewColumn()
+  camp4: string;
 
-    @ViewColumn()
-    camp4: string
+  @ViewColumn()
+  camp5: string;
 
-    @ViewColumn()
-    camp5: string
+  @ViewColumn()
+  camp6: string;
 
-    @ViewColumn()
-    camp6: string
+  @ViewColumn()
+  camp7: string;
 
-    @ViewColumn()
-    camp7: string
+  @ViewColumn()
+  camp8: string;
 
-    @ViewColumn()
-    camp8: string
+  @ViewColumn()
+  camp9: string;
 
-    @ViewColumn()
-    camp9: string
+  @ViewColumn()
+  camp10: string;
 
-    @ViewColumn()
-    camp10: string
+  @ViewColumn()
+  camp11: string;
 
-    @ViewColumn()
-    camp11: string
+  @ViewColumn()
+  camp12: string;
 
-    @ViewColumn()
-    camp12: string
+  @ViewColumn()
+  camp13: string;
 
-    @ViewColumn()
-    camp13: string
+  @ViewColumn()
+  camp14: string;
 
-    @ViewColumn()
-    camp14: string
+  @ViewColumn()
+  camp15: string;
 
-    @ViewColumn()
-    camp15: string
+  @ViewColumn()
+  camp16: string;
 
-    @ViewColumn()
-    camp16: string
+  @ViewColumn()
+  camp17: string;
 
-    @ViewColumn()
-    camp17: string
+  @ViewColumn()
+  camp18: string;
 
-    @ViewColumn()
-    camp18: string
+  @ViewColumn()
+  camp19: string;
 
-    @ViewColumn()
-    camp19: string
+  @ViewColumn()
+  created_at: Date;
 
-    @ViewColumn()
-    created_at: Date
+  @ViewColumn()
+  updated_at: Date;
 
-    @ViewColumn()
-    updated_at: Date
+  @ViewColumn()
+  status: number;
 
-    @ViewColumn()
-    status: number
+  @ViewColumn()
+  result1: string;
 
-    @ViewColumn()
-    result1: string
+  @ViewColumn()
+  result2: string;
 
-    @ViewColumn()
-    result2: string
+  @ViewColumn()
+  result3: string;
 
-    @ViewColumn()
-    result3: string
+  @ViewColumn()
+  result4: string;
 
-    @ViewColumn()
-    result4: string
+  @ViewColumn()
+  result5: string;
 
-    @ViewColumn()
-    result5: string
+  @ViewColumn()
+  result6: string;
 
-    @ViewColumn()
-    result6: string
+  @ViewColumn()
+  viento1: string;
 
-    @ViewColumn()
-    viento1: string
+  @ViewColumn()
+  viento2: string;
 
-    @ViewColumn()
-    viento2: string
+  @ViewColumn()
+  viento3: string;
 
-    @ViewColumn()
-    viento3: string
+  @ViewColumn()
+  viento4: string;
 
-    @ViewColumn()
-    viento4: string
+  @ViewColumn()
+  viento5: string;
 
-    @ViewColumn()
-    viento5: string
+  @ViewColumn()
+  viento6: string;
 
-    @ViewColumn()
-    viento6: string
-
-    @ViewColumn()
-    parcial: string
+  @ViewColumn()
+  parcial: string;
 }
 
 @ViewEntity("view_data_r2_size")
 export class Results_view_m2_size extends BaseEntity {
+  @ViewColumn()
+  size: string;
 
-   
-    @ViewColumn()
-    size: string
-
-    @ViewColumn()
-    id_serie: number
-
+  @ViewColumn()
+  id_serie: number;
 }
 
 @ViewEntity("view_data_results_m2")
 export class Results_view_m2 extends BaseEntity {
+  @ViewColumn()
+  id: number;
 
-    @ViewColumn()
-    id: number
+  @ViewColumn()
+  id_result: number;
 
-    @ViewColumn()
-    id_result: number
-   
-    @ViewColumn()
-    size: string
+  @ViewColumn()
+  size: string;
 
-    @ViewColumn()
-    id_serie: number
+  @ViewColumn()
+  id_serie: number;
 
-    @ViewColumn()
-    r1: string
+  @ViewColumn()
+  r1: string;
 
-    @ViewColumn()
-    r2: string
+  @ViewColumn()
+  r2: string;
 
-    @ViewColumn()
-    r3: string
-
+  @ViewColumn()
+  r3: string;
 }
